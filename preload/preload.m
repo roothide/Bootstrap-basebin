53717a5cc69c39818d69c8099748c259a2c1494d
RootHide <134120506+RootHide@users.noreply.github.com>
2023-12-24 09:16:18 +0800

    fix zebra simulated?

--- preload/preload.m
+++ preload/preload.m
@@ -41,14 +41,23 @@ int main(int argc, char *argv[], char *envp[]) {
 
 		argv[0] = executable;
 		
+//		execv(argv[0], argv); //don't use envp
+
 		posix_spawnattr_t attr;
 		posix_spawnattr_init(&attr);
 
 		#define _POSIX_SPAWN_NANO_ALLOCATOR     0x0200
 		posix_spawnattr_setflags(&attr, POSIX_SPAWN_SETEXEC|POSIX_SPAWN_CLOEXEC_DEFAULT|_POSIX_SPAWN_NANO_ALLOCATOR);
 
+
+		posix_spawn_file_actions_t action;
+		posix_spawn_file_actions_init(&action);
+		posix_spawn_file_actions_addopen(&action, 0, "/dev/null", 0x20000, 0x1b6);
+		posix_spawn_file_actions_addopen(&action, 0x1, "/dev/null", 0x20002, 0x1b6);
+		posix_spawn_file_actions_addopen(&action, 0x2, "/dev/null", 0x20002, 0x1b6);
+
 		pid_t pid=0;
-		int ret = posix_spawn(&pid, argv[0], NULL, &attr, argv, environ);
+		int ret = posix_spawn(&pid, argv[0], &action, &attr, argv, environ);
 		NSLog(@"exec failed %s,%d for %s", strerror(ret), pid, argv[0]);
 		abort();
 	}
